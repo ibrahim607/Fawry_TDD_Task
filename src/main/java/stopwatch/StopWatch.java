f4a1fb0b384681d4979160c786f259c1207f8891
ibrahim607 <himayasin00@gmail.com>
2025-08-12 02:01:34 +0300

    third commit minutes = 60 passed

--- src/main/java/stopwatch/StopWatch.java
+++ src/main/java/stopwatch/StopWatch.java
@@ -10,7 +10,13 @@ public class StopWatch {
         if (minutes >= 0) {
             this.minutes = minutes;
         }
+
+        if (minutes >= 60){
+            this.hours += 1;
+        }
     }
+
+
 
     public int getHours() {
         return hours;
